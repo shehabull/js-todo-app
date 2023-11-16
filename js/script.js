e578a1c8acf601285fdd0c9f671c21fcf288f982
shehabull <shehabull@gmail.com>
2023-11-16 19:20:18 +0600

    refector script

--- js/script.js
+++ js/script.js
@@ -3,129 +3,132 @@ let newTask = document.querySelector("#new-task");
 let items = document.querySelector(".items");
 let completedTask = document.querySelector(".completed-items");
 
-// functions
+// Get all tasks from the local Storage or return an empty array
+
+const getTasksFromLocalStorage = () => {
+  const localStorageItem = localStorage.getItem("tasks");
+  return localStorageItem ? JSON.parse(localStorageItem) : [];
+};
+
+// Sasve task to local Storage
+
+const saveTasksToLocalStorage = (tasks) => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Add New Task
 
 const addTask = (event) => {
   event.preventDefault();
-  const d = new Date();
-  const id = d.getTime();
+
+  const id = new Date().getTime();
   const title = newTask.value;
-  const items = [];
-  const item = { id: id, title: title, status: "incomplete" };
+  const status = "incomplete";
 
-  if (localStorage.getItem("tasks") == null) {
-    items.push(item);
-    localStorage.setItem("tasks", JSON.stringify(items));
-  } else {
-    const localStorageItem = localStorage.getItem("tasks");
-    const newTasks = JSON.parse(localStorageItem);
-    newTasks.push(item);
-    localStorage.setItem("tasks", JSON.stringify(newTasks));
-  }
-  createTask();
+  const tasks = getTasksFromLocalStorage();
+
+  tasks.push({ id, title, status });
+
+  saveTasksToLocalStorage(tasks);
+
+  renderTasksUI();
   newTask.value = "";
 };
 
-const createTask = (event) => {
-  const localStorageItem = JSON.parse(localStorage.getItem("tasks")) || [];
-  let lists = "";
-  localStorageItem.map((item) => {
-    if (item.status == "incomplete") {
-      lists += ` <li class="list-group-item">
+// Create HTML for a task
+const createTaskHTML = (task) => {
+  const { id, title, status } = task;
+
+  return `
+    <li class="list-group-item">
+        <!-- Your HTML structure here -->
+    </li>
+  `;
+};
+
+// Create HTML for incomplete tasks
+const createIncompleteTaskHTML = (task) => {
+  const { id, title } = task;
+
+  return `
+    <li class="list-group-item">
       <div class="group d-flex align-items-center justify-content-between">
-                    <div class="form-check">
-                        <input class="form-check-input" onChange="changeStatus(this)" type="checkbox" value="" id=${item.id}>
-                        <label class="form-check-label" for=${item.id}>
-                            ${item.title}
-                        </label>
-                    </div>
-
-                    <div class="icons">
-                        <i class="bi bi-pencil" onClick="editTitle(this, ${item.id})"></i>
-                    </div>
-                    </div>
-
-                    <div class="d-none input-group"> 
-                        <input type="text" class="form-control" value="${item.title}">
-                        <button class="btn btn-primary" onClick="editTask(this, ${item.id})">Save</button>
-                    </div>
-                </li>
-        `;
-    } else {
-      inCompletedTask();
-    }
-  });
-  items.innerHTML = lists;
-  newTask.value = "";
+        <div class="form-check">
+          <input class="form-check-input" onChange="changeStatus(${id}, this.checked)" type="checkbox" value="" id=${id}>
+          <label class="form-check-label" for=${id}>
+            ${title}
+          </label>
+        </div>
+        <div class="icons">
+          <i class="bi bi-pencil" onClick="editTitle(this, ${id})"></i>
+        </div>
+      </div>
+      <div class="d-none input-group"> 
+        <input type="text" class="form-control" value="${title}">
+        <button class="btn btn-primary" onClick="editTask(${id}, this.previousElementSibling.value)">Save</button>
+      </div>
+    </li>
+  `;
 };
 
-const changeStatus = (e) => {
-  const id = e.id;
-  const status = e.checked ? "completed" : "incomplete";
-  const localStorageItem = JSON.parse(localStorage.getItem("tasks")) || [];
+// Create HTML for completed tasks
+const createCompletedTaskHTML = (task) => {
+  const { id, title } = task;
 
-  let selectedItem = localStorageItem.filter((item) => {
-    return item.id == id;
-  });
-
-  let updatedItem = { ...selectedItem[0] };
-
-  updatedItem.status = status;
-
-  console.log(updatedItem);
-
-  let findIndex = localStorageItem.findIndex((item) => item.id == id);
-  localStorageItem.splice(findIndex, 1, updatedItem);
-
-  localStorage.setItem("tasks", JSON.stringify(localStorageItem));
-
-  createTask();
+  return `
+    <li class="list-group-item d-flex align-items-center justify-content-between">
+      <div class="form-check">
+        <input class="form-check-input" checked type="checkbox" value="" id=${id} disabled="disabled">
+        <label class="form-check-label" for=${id}>
+          ${title}
+        </label>
+      </div>
+      <div class="icons">
+        <i class="bi bi-trash text-danger" onClick="deleteItem(${id})"></i>
+      </div>
+    </li>
+  `;
 };
 
-const inCompletedTask = () => {
-  const localStorageItem = JSON.parse(localStorage.getItem("tasks")) || [];
-  let lists = "";
-  localStorageItem.map((item) => {
-    if (item.status == "completed") {
-      lists += ` <li class="list-group-item d-flex align-items-center justify-content-between">
-   
-                    <div class="form-check">
-                        <input class="form-check-input" checked type="checkbox" value="" id=${item.id} disabled="disabled">
-                        <label class="form-check-label" for=${item.id}>
-                            ${item.title}
-                        </label>
-                    </div>
+const renderTasksUI = () => {
+  const tasks = getTasksFromLocalStorage();
+  const incompleteTasksHTML = tasks
+    .filter((task) => task.status === "incomplete")
+    .map(createIncompleteTaskHTML)
+    .join("");
 
-                    <div class="icons">
-                        <i class="bi bi-trash text-danger"  onClick="deleteItem(${item.id})"></i>
-                    </div>
+  const completedTasksHTML = tasks
+    .filter((task) => task.status === "completed")
+    .map(createCompletedTaskHTML)
+    .join("");
 
-                </li>
-        `;
-    }
-  });
-  completedTask.innerHTML = lists;
+  items.innerHTML = incompleteTasksHTML;
+  completedTask.innerHTML = completedTasksHTML;
 };
 
+// Change task status (completed or incomplete)
+const changeStatus = (id, checked) => {
+  const tasks = getTasksFromLocalStorage();
+
+  const updatedTasks = tasks.map((task) =>
+    task.id === id
+      ? { ...task, status: checked ? "completed" : "incomplete" }
+      : task
+  );
+
+  saveTasksToLocalStorage(updatedTasks);
+  renderTasksUI();
+};
+
+// Delete a task
 const deleteItem = (id) => {
-  const localStorageItem = JSON.parse(localStorage.getItem("tasks")) || [];
+  const tasks = getTasksFromLocalStorage();
 
-  let selectedItem = localStorageItem.filter((item) => {
-    return item.id == id;
-  });
+  const updatedTasks = tasks.filter((task) => task.id !== id);
 
-  let updatedItem = { ...selectedItem[0] };
-
-  let findIndex = localStorageItem.findIndex((item) => item.id == id);
-  localStorageItem.splice(findIndex, 1);
-
-  localStorage.setItem("tasks", JSON.stringify(localStorageItem));
-
-  inCompletedTask();
+  saveTasksToLocalStorage(updatedTasks);
+  renderTasksUI();
 };
-
-//
-
 const editTitle = (element, id) => {
   const divParent = element.closest(".group");
   divParent.style.setProperty("display", "none", "important");
@@ -136,27 +139,17 @@ const editTitle = (element, id) => {
   );
 };
 
-const editTask = (element, id) => {
-  const title = element.previousElementSibling.value;
+// Edit task title
+const editTask = (id, title) => {
+  const tasks = getTasksFromLocalStorage();
 
-  const localStorageItem = JSON.parse(localStorage.getItem("tasks")) || [];
+  const updatedTasks = tasks.map((task) =>
+    task.id === id ? { ...task, title } : task
+  );
 
-  let selectedItem = localStorageItem.filter((item) => {
-    return item.id == id;
-  });
-
-  let updatedItem = { ...selectedItem[0] };
-
-  updatedItem.title = title;
-
-  let findIndex = localStorageItem.findIndex((item) => item.id == id);
-  localStorageItem.splice(findIndex, 1, updatedItem);
-
-  localStorage.setItem("tasks", JSON.stringify(localStorageItem));
-
-  element.previousElementSibling.value = "";
-  createTask();
+  saveTasksToLocalStorage(updatedTasks);
+  renderTasksUI();
 };
 
 form.addEventListener("submit", addTask);
-createTask();
+renderTasksUI();
